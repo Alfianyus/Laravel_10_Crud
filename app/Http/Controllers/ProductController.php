08e600da78a90ea128911047817be110432259d6
alfian.yuska <alfian.yuska@phiraka.com>
2023-06-28 20:19:06 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $product = Product::orderBy('created_at', 'DESC',)->get();
+
+
+        return view('product.index', compact('product'));
+    }
+
+    public function create()
+    {
+        return view('product.create');
+    }
+
+    public function store(Request $request)
+    {
+        Product::create($request->all());
+
+        return redirect()->route('product.index')->with('success', 'product added successfully');
+    }
+
+    public function show(string $id)
+    {
+        $product = Product::findOrFail($id);
+        return view('product.show', compact('product'));
+    }
+
+    public function edit(string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('product.edit', compact('product'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->update($request->all());
+
+        return redirect()->route('product.index')->with('success', 'Product Update successfully');
+    }
+
+    public function destroy(string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->delete();
+
+        return redirect()->route('product.index')->with('success', 'Product deleted Successfully');
+    }
+}
